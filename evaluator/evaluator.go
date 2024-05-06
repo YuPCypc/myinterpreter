dad8ef758d08b052f45c9f3d8f49dba4eb7dc0cb
YuPCypc <838433378@qq.com>
2024-05-06 13:07:20 +0800

    infix eval

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -24,6 +24,10 @@ func Eval(node ast.Node) object.Object {
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
 	}
 	return nil
 }
@@ -74,4 +78,43 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		res = Eval(stmt)
 	}
 	return res
+}
+
+func evalInfixExpression(op string, left, right object.Object) object.Object {
+	switch {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(op, left, right)
+	case op == "==": //==和!=直接进行指针比较
+		return nativeBoolToBooleanObject(left == right)
+	case op == "!=":
+		return nativeBoolToBooleanObject(left != right)
+
+	default:
+		return NULL
+	}
+}
+
+func evalIntegerInfixExpression(op string, left, right object.Object) object.Object {
+	lValue, rValue := left.(*object.Integer).Value, right.(*object.Integer).Value
+
+	switch op {
+	case "+":
+		return &object.Integer{Value: lValue + rValue}
+	case "-":
+		return &object.Integer{Value: lValue - rValue}
+	case "*":
+		return &object.Integer{Value: lValue * rValue}
+	case "/":
+		return &object.Integer{Value: lValue / rValue}
+	case "<":
+		return nativeBoolToBooleanObject(lValue < rValue)
+	case ">":
+		return nativeBoolToBooleanObject(lValue > rValue)
+	case "==":
+		return nativeBoolToBooleanObject(lValue == rValue)
+	case "!=":
+		return nativeBoolToBooleanObject(lValue != rValue)
+	default:
+		return NULL
+	}
 }
